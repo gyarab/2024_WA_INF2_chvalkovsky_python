9f222d9ded09a50670f89be9114b6dafcd32db5e
BrunoChvalkovsky <bruno.chvalkovsky@gmail.com>
2025-01-23 10:07:51 +0100

    django

--- test.py
+++ test.py
@@ -44,13 +44,45 @@ def primes_in_range(a, b):
             primes.append(num)
     return primes
 
-def split_intro_threes(text):
+def split_into_threes(text):
     text = text.replace('"', '')
     if not isinstance(text, str):
         raise ValueError
     return [text[i:i+3] for i in range(0, len(text), 3)]
+
+def caesar_encode(text):
+    alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    shift = 3
+    for i in range(0, len(text) 0 , 1):
+        text = list(text)
+        if text[i].lower() in alph:
+            text[i] = alph[(alph.index(text[i].lower()) + shift) % 26]
+        text = ''.join(text)
+        return text
+    
+def morse(text):
+    morse_code = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+        '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+        '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
+    }
+    for c in text.upper():
+        if c not in morse_code:
+            raise ValueError(f"Character '{c}' cannot be encoded in Morse code.")
+        for char in text:
+            if char in text:
+                if char == ' ':
+                    text = text.replace(char, ' / ')
+                morse_code.index(char)
+
+
+        return ' '.join([morse_code[c] for c in text.upper()])
 if __name__ == "__main__":
     print(fibonacci(n))
     print(is_prime(6))
     print(primes_in_range(20, 1))
-    print(split_intro_threes("aj dnjasnd ansdioa j"))
+    print(split_into_threes("aj dnjasnd ansdioa j"))
+    print(caesar_encode("abc"))
+    print(morse("test"))
+
+    
