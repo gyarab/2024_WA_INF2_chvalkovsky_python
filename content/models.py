7e331bfdf5e5c953205b3a340e13e7bd274faf8f
BrunoChvalkovsky <bruno.chvalkovsky@gmail.com>
2025-03-30 14:44:56 +0200

    genres M:N added

--- content/models.py
+++ content/models.py
@@ -6,10 +6,15 @@ class Brand(models.Model):
     origin = models.CharField(max_length=20, blank=False, null=False)
     info = models.TextField(blank=False, null=False)
 
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False)
 
     def __str__(self):
         return self.name
-    
+
 class Guitar(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     pickups = models.CharField(max_length=5, blank=False, null=False)
@@ -18,7 +23,7 @@ class Guitar(models.Model):
     image = models.URLField(blank=False, null=False)
     description = models.TextField(blank=False, null=False)
     shape = models.CharField(max_length=30, blank=False, null=False)
+    genre = models.ManyToManyField(Genre, related_name="guitars", blank=True)
 
     def __str__(self):
         return f"{self.brand} {self.model}"
-    
