3b43424923b65f23c9700cc7840df44b649c338f
bruno <48710651+BrunoChvalkovsky@users.noreply.github.com>
2025-04-24 11:20:15 +0200

    Delete staticfiles directory

--- staticfiles/content/js/details.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var guitarImage = document.querySelector(".guitar-image");
-        guitarImage.onclick = function() {
-            this.classList.toggle("large");
-        };
-});
